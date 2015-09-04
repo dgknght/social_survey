4368d0999438abe6060308661164d55d9b15e8f4
Doug Knight <doug.knight@livingsocial.com>
2015-09-03 20:46:18 -0500

    add SurveysController shell and :create test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'surveys/index'
+
+  get 'surveys/create'
+
+  get 'surveys/new'
+
+  get 'surveys/edit'
+
+  get 'surveys/update'
+
+  get 'surveys/destroy'
+
+  get 'surveys/show'
+
   devise_for :users
   root to: 'pages#welcome'
   resources :surveys
