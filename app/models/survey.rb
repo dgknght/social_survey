5a162fbf5425fe9785905b372edd942f5aa2a7d0
Doug Knight <doug.knight@livingsocial.com>
2015-09-06 17:06:31 -0500

    require Survey#user_id

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,5 @@ class Survey < ActiveRecord::Base
   validates :name, presence: true,
                    length: { maximum: 50 },
                    uniqueness: { scope: :user_id }
+  validates_presence_of :user_id
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Survey, type: :model do
+  let (:user) { FactoryGirl.create(:user) }
   let (:survey_attributes) do
     {
       name: 'My first survey',
-      description: 'This is a my very cool survey'
+      description: 'This is a my very cool survey',
+      user_id: user.id
     }
   end
 
@@ -33,6 +35,14 @@ RSpec.describe Survey, type: :model do
       survey_2 = Survey.new(survey_attributes)
       expect(survey_2).not_to be_valid
       expect(survey_2).to have_at_least(1).error_on(:name)
+    end
+  end
+
+  describe '#user_id' do
+    it 'is required' do
+      survey = Survey.new(survey_attributes.except(:user_id))
+      expect(survey).not_to be_valid
+      expect(survey).to have_at_least(1).error_on(:user_id)
     end
   end
 end
