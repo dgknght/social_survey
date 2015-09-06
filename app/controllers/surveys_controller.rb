ee0bb94a9b0c89493268833dfce57e5be772c447
Doug Knight <doug.knight@livingsocial.com>
2015-09-06 16:09:38 -0500

    [#102634444] implement SurveysController#create

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,11 +1,17 @@
 class SurveysController < ApplicationController
+  respond_to :html
+
   def index
   end
 
   def create
+    @survey = Survey.new(survey_params)
+    flash[:notice] = 'The survey was created successfully.' if @survey.save
+    respond_with @survey, location: surveys_path
   end
 
   def new
+    @survey = Survey.new
   end
 
   def edit
@@ -18,5 +24,11 @@ class SurveysController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:name, :description)
   end
 end
